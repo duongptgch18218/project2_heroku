8944fc0fa4af0cfb4437a86599a9595a25aa1a7c
duongptgch18218 <duongptgch18218@fpt.edu.vn>
2020-05-20 10:36:18 +0700

    change second

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>Profile</h1>
 <h2>Name: Phan Tung Duong</h2>
 
-<h2>Picture: <a href="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" width = "500px" height="700px"></a></h2>
+<h2>Picture: <a href="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"></a></h2>
 
 </body>
 </html>
