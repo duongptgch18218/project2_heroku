74eebfbacfb3630c8f8f54da2e148bb6889120b2
duongptgch18218 <50705423+duongptgch18218@users.noreply.github.com>
2020-05-20 10:44:03 +0700

    Change size image

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <h2>Name: Phan Tung Duong</h2>
 
 <h2>Picture: </h2>
-<a style="padding-left: 50px" href="https://www.facebook.com/phan.duong.3591267"> <img src="https://scontent.fhan2-2.fna.fbcdn.net/v/t1.0-9/s960x960/92953562_1114125735603036_8439316076113690624_o.jpg?_nc_cat=111&_nc_sid=85a577&_nc_ohc=WoMg7covM0MAX9A4uWT&_nc_ht=scontent.fhan2-2.fna&_nc_tp=7&oh=dc9106f765c21f027ab9e814fe1d83c7&oe=5EE95AAE" alt=""> </a>
+<a style="padding-left: 50px" href="https://www.facebook.com/phan.duong.3591267"> <img src="https://scontent.fhan2-2.fna.fbcdn.net/v/t1.0-9/s960x960/92953562_1114125735603036_8439316076113690624_o.jpg?_nc_cat=111&_nc_sid=85a577&_nc_ohc=WoMg7covM0MAX9A4uWT&_nc_ht=scontent.fhan2-2.fna&_nc_tp=7&oh=dc9106f765c21f027ab9e814fe1d83c7&oe=5EE95AAE" width="500px" height="700px" alt=""> </a>
 
 </body>
 </html>
